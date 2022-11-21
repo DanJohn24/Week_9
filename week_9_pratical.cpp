5e08d0dd825e2abb7453c5bb60eba59a50e7f887
Daniel <19026845@brookes.ac.uk>
2022-11-21 11:57:30 +0000

    first commit

---
+++ week_9_pratical.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+#include <mariadb/conncpp.hpp>
+
+using namespace std;
+using namespace cgicc;
+
+int main(){
+    Cgicc test;
+ 
+    test.getElements().size();
+    
+
+    
+}
